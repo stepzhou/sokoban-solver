26e85b3ffec71a90a42f6878455d67e6f998f9bb
Stephen Zhou <step.zhou@gmail.com>
2013-10-21 01:58:05 -0400

    can solve sophia and kristi (slow), starting uniform cost

---
+++ src/UniformCostSolver.java
@@ -0,0 +1,13 @@
+
+public class UniformCostSolver extends AbstractSolver {
+	
+	public UniformCostSolver(BoardState initialState) {
+		super(initialState);
+	}
+
+	@Override
+	String search() throws NoSolutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
